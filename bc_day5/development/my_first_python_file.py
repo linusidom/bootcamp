c5495fe3c7f288d17bd9c92c45e79dd0290ce86f
linusidom <linusidom@gmail.com>
2020-04-06 15:14:56 +0700

    Bootcamp Day 5 numbers and strings

---
+++ bc_day5/development/my_first_python_file.py
@@ -0,0 +1,120 @@
+# This is a comment
+# print('hello world') #This is also a comment
+
+# print('Use the print() function to print to the screen')
+
+"""
+Numbers
+"""
+# numbers - integers, floats
+# intergers = 5,7,10,135,450
+# floats = 5.0, 6.3, 13242.23423423423
+
+# print('The number 5 is of type ', type(5))
+# print('The number 5.4 is of type ',type(5.4))
+
+# Arithmetic
+# + - Plus
+# - - Minus
+# / - Divide
+# // - Floor Division
+# % - Modulus - Remainder
+# ** - Exponent
+
+# print(5+5)
+# print(5-5)
+# print(5--5)
+# print(5/5)
+# print(5.0+5)
+# # print(5/0)
+# print('Number Division', 5/3)
+# print('Number Division', 5.0/3)
+# print('Floor Division',5//3)
+# print('Modulus Remainder of 2',5%3)
+# print('Modulus Remainder of 1', 10%3)
+# print('Exponent', 5**5)
+
+# Order of Operations
+# PEMDAS
+# Parentheses
+# Exponent
+# Multiplication
+# Division
+# Addition
+# Subtration
+
+# total_meal = 100
+# service_charge = 0.10
+# total_bill = total_meal * 1 + service_charge
+# print('Incorrect Your Total Bill is:',total_bill)
+
+
+# total_meal = 100
+# service_charge = 0.10
+# total_bill = total_meal * (1 + service_charge)
+# print('Your Total Bill is:',('%.2f' % total_bill))
+
+print(dir(5))
+
+
+"""
+Strings
+"""
+# my_string = 'This is a string'
+# string_index = 'String Indexing Starts at 0!!!!'
+# print('This is the third letter in the String ',my_string[2])
+
+my_string = 'The Quick Brown Fox Jumped Over The Fence'
+
+# Substrings
+# print(my_string[0:2])
+# print(my_string[1:-1])
+# print('Print Every Letter in Reverse' ,my_string[::-1])
+# print('Print Every Other Letter in Reverse', my_string[::-2])
+
+
+# my_string = 'The Quick Brown Dog Jumped Over The Fence'
+# print(my_string)
+# print(my_string.split())
+# my_new_string = my_string.split()
+
+# print(my_new_string,my_string)
+
+
+# Strings and Numbers and IMMUTABLE - CANNOT BE CHANGED
+# If You want to CHANGE Must RE-ASSIGN
+
+# print(my_string.upper())
+# print(my_string.lower())
+# print(my_string.capitalize())
+# print(my_string.title())
+
+# Ready Made Fucntions available for variables of Type String
+# print(type(my_string))
+# print(dir(my_string))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Lists
+"""
+
+
+
+
+"""
+Dictionaries
+"""
