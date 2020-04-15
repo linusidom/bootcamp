43c47d789a2b01ea6e502f3f5a229c613b2f4462
linusidom <linusidom@gmail.com>
2020-04-15 15:08:43 +0700

    Bootcamp Day 12 Connected Templates and Static Files, added firstapp and secondapp and showed template tagging between pages

---
+++ bc_day12/development/firstproject/firstapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+app_name = 'firstapp'
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='firstapp/index.html'), name='index'),
+    
+]
